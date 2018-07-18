f567fbaaab5a31b97b9e81ffd2780c3c1392b90b
Rick Payne <rickp@rossfell.co.uk>
2018-07-18 15:21:49 +1200

    Minor bridge name change
    Bridge name change on my laptop..

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-qemu-system-x86_64 -m 6G -smp 2 --nographic -gdb tcp::1234,server,nowait \
+qemu-system-x86_64 -m 2G -smp 2 --nographic -gdb tcp::1234,server,nowait \
     -device virtio-blk-pci,id=blk0,bootindex=0,drive=hd0,scsi=off \
     -drive file=_build/osv_build/cowboy_example.img,if=none,id=hd0,cache=none,aio=native \
-    -netdev bridge,id=hn0,br=br0,helper=/usr/lib/qemu/qemu-bridge-helper \
+    -netdev bridge,id=hn0,br=virbr0,helper=/usr/lib/qemu/qemu-bridge-helper \
     -device virtio-net-pci,mac=00:11:11:11:11:01,netdev=hn0,id=nic0 \
     -device virtio-rng-pci -enable-kvm -cpu host,+x2apic \
     -chardev stdio,mux=on,id=stdio,signal=off -mon chardev=stdio,mode=readline,default \
